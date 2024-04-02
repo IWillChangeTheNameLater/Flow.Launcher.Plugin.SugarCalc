560f0d04ed282d74c3ba3cc0c9f54d931653f6f5
IWillChangeTheNameLater <93054174+IWillChangeTheNameLater@users.noreply.github.com>
2024-04-02 19:54:25 +0500

    Make plugin settings available

--- plugin/sugar_calc.py
+++ plugin/sugar_calc.py
@@ -2,6 +2,15 @@ from flowlauncher import FlowLauncher
 
 
 class SugarCalc(FlowLauncher):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.show_history = self.settings.get('show_history')
+        self.latest_result_char = self.settings.get('latest_result_char')
+        self.number_of_decimals = self.settings.get('number_of_decimals')
+        self.rounding_direction = self.settings.get('rounding_direction')
+        self.signs_aliases = self.settings.get('signs_aliases')
+
     def query(self, query='') -> list:
         return [
             {
